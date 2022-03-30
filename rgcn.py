7be28c22548fb0c30bfcb6c452c28da9fde03e7e
Yan Ding <dingyanleon@gmail.com>
2022-03-30 21:14:29 +0000

    Revision_2

--- rgcn.py
+++ rgcn.py
@@ -52,26 +52,43 @@ def model_train(train_idx):
 
 
 @torch.no_grad()
-def model_test(test_idx):
+def model_val(val_idx, plus_test=False):
     model.eval()
     out, emb = model(data.x, data.edge_index, data.edge_type)
-    out = out.exp()[test_idx]
-    pred = out.argmax(dim=-1)
-    acc = accuracy_score(data.y[test_idx].cpu(), pred.cpu())
-    auroc = roc_auc_score(data.y[test_idx].cpu(), out[:, 1].cpu())
-    auprc = average_precision_score(
-            data.y[test_idx].cpu(), out[:, 0].cpu(), pos_label=0)
-    sens = recall_score(data.y[test_idx].cpu(), pred.cpu())
-    spec = recall_score(data.y[test_idx].cpu(), pred.cpu(), pos_label=0)
-    mcc = matthews_corrcoef(data.y[test_idx].cpu(), pred.cpu())
-    return acc, sens, spec, mcc, auroc, auprc
-
+    out_val = out.exp()[val_idx]
+    pred_val = out_val.argmax(dim=-1)
+    auroc_val = roc_auc_score(data.y[val_idx].cpu(), out_val[:, 1].cpu())
+    
+    if plus_test == False:
+        acc_val = accuracy_score(data.y[val_idx].cpu(), pred_val.cpu())
+        auprc_val = average_precision_score(
+                data.y[val_idx].cpu(), out_val[:, 0].cpu(), pos_label=0)
+        sens_val = recall_score(data.y[val_idx].cpu(), pred_val.cpu())
+        spec_val = recall_score(data.y[val_idx].cpu(), pred_val.cpu(), pos_label=0)
+        mcc_val = matthews_corrcoef(data.y[val_idx].cpu(), pred_val.cpu())
+        return acc_val, sens_val, spec_val, mcc_val, auroc_val, auprc_val
+    
+    else:
+        out_test = out.exp()[test_index]
+        pred_test = out_test.argmax(dim=-1)
+        acc_test = accuracy_score(data.y[test_index].cpu(), pred_test.cpu())
+        auroc_test = roc_auc_score(data.y[test_index].cpu(), out_test[:, 1].cpu())
+        auprc_test = average_precision_score(
+                data.y[test_index].cpu(), out_test[:, 0].cpu(), pos_label=0)
+        sens_test = recall_score(data.y[test_index].cpu(), pred_test.cpu())
+        spec_test = recall_score(data.y[test_index].cpu(), pred_test.cpu(), pos_label=0)
+        mcc_test = matthews_corrcoef(data.y[test_index].cpu(), pred_test.cpu())
+        return auroc_val, acc_test, sens_test, spec_test, mcc_test, auroc_test, auprc_test
 
 data = torch.load('graph.pt')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 data = data.to(device)
-sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=8)
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.15, random_state=661)
 train_index, test_index = next(sss.split(data.x.cpu(), data.y.cpu()))
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.15/0.85, random_state=154)
+train_slice, val_slice = next(sss.split(data.x[train_index].cpu(), data.y[train_index].cpu()))
+val_index = train_index[val_slice]
+train_index = train_index[train_slice]
 skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=18)
 
 # Hyperparameters
@@ -106,10 +123,13 @@ for train_mask, val_mask in tqdm(skf.split(data.x[train_index].cpu(), data.y[tra
     optimizer = PCGrad(torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay))
 
     auroc_max = 0
-    for epoch in range(1000):
+    epoch_count = 0
+    for epoch in range(500):
+        epoch_count += 1
         model_train(train_index[train_mask])
-        acc, sens, spec, mcc, auroc, auprc = model_test(train_index[val_mask])
+        acc, sens, spec, mcc, auroc, auprc = model_val(train_index[val_mask])
         if auroc > auroc_max:
+            epoch_count = 0
             acc_max = acc
             auroc_max = auroc
             auprc_max = auprc
@@ -117,6 +137,8 @@ for train_mask, val_mask in tqdm(skf.split(data.x[train_index].cpu(), data.y[tra
             spec_max = spec
             mcc_max = mcc
             epoch_max = epoch
+        if epoch_count == 30:
+            break
 
     acc_list.append(acc_max)
     sens_list.append(sens_max)
@@ -137,11 +159,15 @@ print('Testing progressing...')
 model = Net(dim1, dim2, dim3, dropout).to(device)
 optimizer = PCGrad(torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay))
 
-auroc_max = 0
-for epoch in range(1000):
+auroc_val_max = 0
+epoch_count = 0
+for epoch in range(500):
+    epoch_count += 1
     model_train(train_index)
-    acc, sens, spec, mcc, auroc, auprc = test(test_index)
-    if auroc > auroc_max:
+    auroc_val, acc, sens, spec, mcc, auroc, auprc = model_val(val_index, plus_test=True)
+    if auroc_val > auroc_val_max:
+        epoch_count = 0
+        auroc_val_max = auroc_val
         acc_max = acc
         auroc_max = auroc
         auprc_max = auprc
@@ -149,7 +175,9 @@ for epoch in range(1000):
         spec_max = spec
         mcc_max = mcc
         epoch_max = epoch
-
+    if epoch_count == 30:
+        break
+        
 test_result = pd.Series([acc_max, sens_max, spec_max, mcc_max, auroc_max, auprc_max],
                         index=['Accuracy', 'Sensitivity', 'Specificity', 'MCC', 'AUROC', 'AUPRC'])
 print('Testing results at Epoch {}:'.format(epoch_max))
